4e492b0cadc074dcc490024bbcfbc3c6cb431129
uta8a <noumodulity@gmail.com>
2021-07-10 21:48:15 +0900

    make simple IndexPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,32 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Image from 'next/image';
+import { GetStaticProps, NextPage } from 'next';
+import path from 'path';
+import fs from 'fs-extra';
+import Link from 'next/link'
 
-export default function Home() {
+export const getStaticProps: GetStaticProps = async () => {
+  const targetDir = path.join(process.cwd(), 'pages', 'post');
+  const dirs = await fs.readdir(targetDir);
+  const paths = [];
+  for (const slug of dirs) {
+    paths.push({ path: slug });
+  }
+  paths.sort();
+  return { props: { path: paths } };
+};
+type PostName = {
+  path: string;
+};
+type Props = {
+  path: PostName[];
+};
+const IndexPage: NextPage<Props> = (props: Props)=> {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={500} height={500} />
-        </a>
-      </footer>
-    </div>
+    <>
+      {props.path.map(e => <Link key={e.path} href={`post/${e.path.replace(/\.tsx/, '')}`}>
+        <a>{e.path.replace(/\.tsx/, '')}</a>
+      </Link>)}
+    </>
   );
 }
+
+export default IndexPage;
